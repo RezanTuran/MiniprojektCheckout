ae077c7da8d4df748c9da9ee1b82a255a52f8308
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2020-06-05 18:34:48 +0200

    ProductCard,Footer are fixed and files is structured

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,18 @@
 import React from 'react';
+import Nav from './components/Nav';
 import Header from './components/Header';
-import { makeStyles } from '@material-ui/core';
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import { Drawer, List, ListItem, ListItemIcon,ListItemText, Container, Typography } from "@material-ui/core";
-import HomeIcon from "@material-ui/icons/Home";
-import InfoIcon from '@material-ui/icons/Info';
-import StoreIcon from '@material-ui/icons/Store';
-import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
-  const classes = useStyles();
   return (
-    <Router>
+    <div>
       <Header />
-        <div style={{display: 'flex'}} >
-            <Drawer
-              style={{ width: '220px' }}
-              variant="persistent"
-              anchor="left"
-              open={true}
-              classes={{ paper: classes.drawerPaper }}
-            >
-              <List>
-                <Link to="/" className={classes.link}>
-                  <ListItem button>
-                    <ListItemIcon>
-                      <HomeIcon />
-                    </ListItemIcon>
-                    <ListItemText primary={"Hem"} />
-                  </ListItem>
-                </Link>
+      <Nav />
+      <Footer />
+    </div>
 
-                <Link to="/product" className={classes.link}>
-                  <ListItem button>
-                    <ListItemIcon>
-                      <StoreIcon />
-                    </ListItemIcon>
-                    <ListItemText primary={"Produkter"} />
-                  </ListItem>
-                </Link>
-
-                <Link to="/about" className={classes.link}>
-                  <ListItem button>
-                    <ListItemIcon>
-                      <InfoIcon />
-                    </ListItemIcon>
-                    <ListItemText primary={"Om Oss"} />
-                  </ListItem>
-                </Link>
-              </List>
-            </Drawer>
-
-            <Switch>
-              <Route exact path ="/" >
-                <Container>
-                  <Typography variant="h3" gutterBottom>
-                    Hem
-                  </Typography>
-                </Container>
-              </Route>
-
-              <Route exact path ="/product" >
-                <Container>
-                  <Typography variant="h3" gutterBottom>
-                    Produkter
-                  </Typography>
-                      <Content /> 
-                </Container>
-              </Route>
-
-              <Route exact path ="/about" >
-                <Container>
-                  <Typography variant="h3" gutterBottom>
-                    Om oss
-                  </Typography>
-                </Container>
-              </Route>
-            </Switch>
-
-        </div>
-    </Router>
   );
 };
-
-
-const useStyles = makeStyles((theme) =>({
-  drawerPaper:{ 
-     width: 'inherit',
-     position: 'initial'
-},
-  link: {
-    textDecoration: 'none', 
-    color: theme.palette.text.primary
-}
-}))
 
 
 export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { Drawer, List, ListItem, ListItemIcon,ListItemText, Container, Typography } from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import InfoIcon from '@material-ui/icons/Info';
+import StoreIcon from '@material-ui/icons/Store';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
+import Contact from '../pages/Contact';
+import Product from '../components/Product';
+import About from '../pages/About';
+import Home from '../pages/Home';
+
+const Nav = () => {
+    const classes = useStyles();
+    return(
+        <Router>
+          <div style={{display: 'flex', height:'200vh'}} >
+              <Drawer
+                variant="persistent"
+                anchor="left"
+                open={true}
+                classes={{ paper: classes.drawerPaper }}
+              >
+                <List>
+                  <Link to="/" className={classes.link}>
+                    <ListItem button>
+                      <ListItemIcon>
+                        <HomeIcon />
+                      </ListItemIcon>
+                      <ListItemText primary={"Hem"} />
+                    </ListItem>
+                  </Link>
+  
+                  <Link to="/product" className={classes.link}>
+                    <ListItem button>
+                      <ListItemIcon>
+                        <StoreIcon />
+                      </ListItemIcon>
+                      <ListItemText primary={"Produkter"} />
+                    </ListItem>
+                  </Link>
+  
+                  <Link to="/about" className={classes.link}>
+                    <ListItem button>
+                      <ListItemIcon>
+                        <InfoIcon />
+                      </ListItemIcon>
+                      <ListItemText primary={"Om Oss"} />
+                    </ListItem>
+                  </Link>
+  
+                  <Link to="/contact" className={classes.link}>
+                    <ListItem button>
+                      <ListItemIcon>
+                        <ContactSupportIcon />
+                      </ListItemIcon>
+                      <ListItemText primary={"Kontakt"} />
+                    </ListItem>
+                  </Link>
+                </List>
+              </Drawer>
+  
+              <Switch>
+                <Route exact path ="/" >
+                  <Container>
+                    <Typography variant="h3" gutterBottom>
+                        <Home />
+                    </Typography>
+                  </Container>
+                </Route>
+  
+                <Route exact path ="/product" >
+                  <Container>
+                    <Typography variant="h3" gutterBottom style={{textAlign: 'center'}}>
+                      Produkter
+                    </Typography>
+                       <Product />
+                  </Container>
+                </Route>
+  
+                <Route exact path ="/about" >
+                  <Container>
+                    <Typography variant="h3" gutterBottom>
+                        <About />
+                    </Typography>
+                  </Container>
+                </Route>
+  
+                <Route exact path ="/contact" >
+                  <Container>
+                    <Typography variant="h3" gutterBottom>
+                        <Contact />
+                    </Typography>
+                  </Container>
+                </Route>
+              </Switch>
+  
+          </div>
+      </Router>
+    );
+};
+
+const useStyles = makeStyles((theme) =>({
+    drawerPaper:{ 
+       width: 'inherit',
+       position: 'initial'
+  },
+    link: {
+      textDecoration: 'none', 
+      color: theme.palette.text.primary
+  }
+  }))
+
+
+export default Nav;
