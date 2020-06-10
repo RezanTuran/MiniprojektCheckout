b5ce576cf144536aea0a7ae9834580843513dc4d
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2020-06-10 18:19:23 +0200

    swish and faktura components is fixed

---
+++ src/components/InvoicePaymentForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const InvoicePaymentForm = () => {
+    return(
+        <h1>Faktura</h1>
+    );
+};
+
+export default InvoicePaymentForm;
---
+++ src/components/SwishPaymentForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const SwishPaymentForm = () => {
+    return(
+       <h1>sad</h1>
+    );
+};
+
+
+
+export default SwishPaymentForm;
