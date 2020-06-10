f070564f6cbd30a4fbd227ab07cc2b6f565bec6b
Rasmus1996 <sebbegrymen@gmail.com>
2020-06-10 14:10:08 +0200

    updated my package json

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,107 +1,107 @@
 import React, { CSSProperties } from 'react';
 import { Button } from '@material-ui/core';
 
-export  interface Product{
+export interface Product {
   id: number
   name: string
   description: string
-  size:number
+  size: number
   price: number
   img: string
 }
 
 let Products: Product[] = [
-  { 
+  {
     "id": 0,
-    "name":"Gant",
+    "name": "Gant",
     "description": "Bra kvalite",
     "size": 41,
     "price": 899,
     "img": "Gant.png"
   },
-  { 
+  {
     "id": 1,
-    "name":"Nike",
+    "name": "Nike",
     "description": "Bra kvalite",
     "size": 43,
     "price": 589,
     "img": "Nike.png"
   },
-  { 
+  {
     "id": 2,
-    "name":"Addidas",
+    "name": "Addidas",
     "description": "Bra kvalite",
     "size": 38,
     "price": 675,
     "img": "Addidas.png"
   },
-  { 
+  {
     "id": 3,
-    "name":"Puma",
+    "name": "Puma",
     "description": "Bra kvalite",
     "size": 41,
     "price": 499,
     "img": "Puma.png"
   },
-  { 
+  {
     "id": 4,
-    "name":"Nike",
+    "name": "Nike",
     "description": "Bra kvalite",
     "size": 40,
     "price": 359,
     "img": "Nike2.png"
   },
-  { 
+  {
     "id": 5,
-    "name":"Puma",
+    "name": "Puma",
     "description": "Bra kvalite",
     "size": 40,
     "price": 359,
     "img": "Puma2.png"
   },
-  { 
+  {
     "id": 6,
-    "name":"Lacoste",
+    "name": "Lacoste",
     "description": "Bra kvalite",
     "size": 40,
     "price": 359,
     "img": "Nike3.png"
   },
-  { 
+  {
     "id": 7,
-    "name":"Gant",
+    "name": "Gant",
     "description": "Bra kvalite",
     "size": 41,
     "price": 899,
     "img": "Gant.png"
   },
-  { 
+  {
     "id": 8,
-    "name":"Gant",
+    "name": "Gant",
     "description": "Bra kvalite",
     "size": 41,
     "price": 899,
     "img": "Gant.png"
   },
-  { 
+  {
     "id": 9,
-    "name":"Puma",
+    "name": "Puma",
     "description": "Bra kvalite",
     "size": 40,
     "price": 359,
     "img": "Puma2.png"
   },
-  { 
+  {
     "id": 10,
-    "name":"Puma",
+    "name": "Puma",
     "description": "Bra kvalite",
     "size": 41,
     "price": 499,
     "img": "Puma.png"
   },
-  { 
+  {
     "id": 11,
-    "name":"Nike",
+    "name": "Nike",
     "description": "Bra kvalite",
     "size": 43,
     "price": 589,
@@ -112,52 +112,52 @@ let Products: Product[] = [
 
 
 const Product = () => {
-  
-    return(
-      <div style={productCardContainer}>
-         {Products.map((product) =>{
-            return(
-            <div style={productCard}>
-              <img style={imgStyle} src={ require("./../assets/images/" + product.img) } alt="produktImg" />
-              {/* <p>{ product.id }</p> */}
-              <h3 style={{color: 'red'}}>{ product.name }</h3>
-              <p style={price}>{ product.price } :- </p>
-              <p>{ product.description }</p>
-              <p>{ product.size }</p>
-              <Button style={button} variant="contained" color="primary">KÖP</Button>
-            </div>
-            )
-          })}
-      </div>
-);
+
+  return (
+    <div style={productCardContainer}>
+      {Products.map((product, index) => {
+        return (
+          <div key={index} style={productCard}>
+            <img style={imgStyle} src={require("./../assets/images/" + product.img)} alt="produktImg" />
+            {/* <p>{ product.id }</p> */}
+            <h3 style={{ color: 'red' }}>{product.name}</h3>
+            <p style={price}>{product.price} :- </p>
+            <p>{product.description}</p>
+            <p>{product.size}</p>
+            <Button style={button} variant="contained" color="primary">KÖP</Button>
+          </div>
+        )
+      })}
+    </div>
+  );
 };
 
-let productCard:CSSProperties ={
+let productCard: CSSProperties = {
   boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
   maxWidth: '300px',
   margin: 'auto',
   marginTop: '20px',
   textAlign: 'center',
   fontFamily: 'arial',
-  
+
 }
 
-let productCardContainer:CSSProperties  ={
-    display: 'flex',
-    flexWrap: 'wrap',
+let productCardContainer: CSSProperties = {
+  display: 'flex',
+  flexWrap: 'wrap',
 }
 
-let imgStyle:CSSProperties ={
-    width: '100%',
-    height: '200px'
+let imgStyle: CSSProperties = {
+  width: '100%',
+  height: '200px'
 }
 
-let price:CSSProperties ={
+let price: CSSProperties = {
   color: 'gray',
   fontSize: '22px'
 }
 
-let button:CSSProperties ={
+let button: CSSProperties = {
   width: '100%',
   borderRadius: '0'
 }
