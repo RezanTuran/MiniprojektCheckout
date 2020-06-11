143647b5722b1cf74ae965848d48d6761e610c14
robi72 <robert.grahovac@hotmail.com>
2020-06-11 18:58:31 +0200

    update shipping

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PaymentForm from '../components/PaymentForm';
 import AddressForm from '../components/AddressForm';
 import { FormControl } from '@material-ui/core';
+import Delivery from '../components/Delivery';
 
 
 const Checkout = () => {
     return(
         <FormControl>
             <AddressForm />
+            <Delivery/>
             <PaymentForm />
         </FormControl>
     );
