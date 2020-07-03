51a8a8d4dbb6ae7c88eaa02f9bb0d05186dd348c
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2020-07-03 20:20:32 +0200

    bugs are fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div style={{backgroundColor:'white'}}>
       <BrowserRouter>
         <CartProvider>
           <Navbar />
--- src/components/pages/Done.tsx
+++ src/components/pages/Done.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
+export  interface DonePageImg{
+    img: string
+  }
+  
 
+  export const Products = [
+    { 
+      "img": "gif.gif"
+    },
+]
 
 const Done = () => {
     return (
         <div>
-            <h1>Tack för att du handlas hos oss..</h1>
-            <h2>Välkommen åter!</h2>
+            <div style={{display:'flex',flexDirection:'column',alignItems:'center'}}>
+                <h5>Tack för att du handlas hos oss..</h5>
+                <h6>Välkommen åter!</h6>
+            </div>
+            <img style={{width:'100%'}} src={ require("../../assets/images/" + Products[0].img) } alt="produktImg" />
         </div>
     );
 };
